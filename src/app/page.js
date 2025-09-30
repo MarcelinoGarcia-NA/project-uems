764260d5f11140d0a13d782347e2425b22ce62d7
Marcelino-ifms <140651566+Marcelino-ifms@users.noreply.github.com>
2025-09-30 19:21:57 -0400

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,48 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Page() {
+  const [nome, setNome] = useState("");
+  const [matricula, setMatricula] = useState("");
+  const [curso, setCurso] = useState("");
+
+  const salvarDados = async(event) => {
+      try{
+         event.preventDefault();
+         const resposta = await fetch("http://localhost:3000/api/alunos", {
+            method: "POST",
+            headers: { "Content-type": "Application/json"},
+            body: JSON.stringify({ nome: nome, matricula: matricula, curso: curso})
+         });
+         if(resposta.ok){
+            alert("Show na bagaça, Layan vai reprovar!");
+         }
+      }catch(err){
+          return console.error("Deu erro na bagaça", err);7
+      }
+  }
+
+  return (
+    <main>
+      <form onSubmit={salvarDados}>
+        <input
+          type="text"
+          placeholder="Digite nome"
+          onChange={(event) => setNome(event.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Digite matrícula"
+          onChange={(event) => setMatricula(event.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Digite curso"
+          onChange={(event) => setCurso(event.target.value)}
+        />
+        <button>Salvar</button>
+      </form>
+    </main>
+  );
+}
